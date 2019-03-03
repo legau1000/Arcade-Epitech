7d5d7af027ae8bef377c77c1ba9f96c623210315
Florian <florian.gau@epitech.eu>
2019-03-03 02:33:43 +0100

    architecture of

---
+++ games/game_1/include/def.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2018
+** Duo Stumper
+** File description:
+** def.h
+*/
+
+#ifndef DEF_HPP__
+#define DEF_HPP__
+
+#define UNUSED __attribute__((unused))
+#define UINT unsigned int
+#define FILE __FILE__
+#define LINE __LINE__
+#define TIME __TIME__
+#define DATE __DATE__
+
+#define V_INT(x) if (!x) return (84)
+#define V_CHAR(x) if (!x) return (NULL)
+
+#endif
---
+++ games/game_1/include/my.hpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2018
+** Duo Stumper
+** File description:
+** my.h
+*/
+
+#ifndef CORE_HPP__
+#define CORE_HPP__
+
+#endif
